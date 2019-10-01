f76fe1843f3e908054f12bc421c4210ec0989a0c
haris <hareluuu@gmail.com>
2019-10-01 19:35:49 +0200

    Initial npm working setup

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl =
+    "mongodb+srv://haris:GeAVb8bFKUzW99@cluster0-0n3t6.mongodb.net/phonebook?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/blogs", (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs);
+    });
+});
+
+app.post("/api/blogs", (request, response) => {
+    const blog = new Blog(request.body);
+    console.log(request.body)
+
+    blog.save().then(result => {
+        response.status(201).json(result);
+    });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
