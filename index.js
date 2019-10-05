ca7f390750726fb389e57cd810121ceb2d22bc02
haris <hareluuu@gmail.com>
2019-10-05 10:22:29 +0200

    Finish part 4

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./app");
+const http = require("http");
+const config = require("./utils/config");
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-});
+const server = http.createServer(app);
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl =
-    "mongodb+srv://haris:GeAVb8bFKUzW99@cluster0-0n3t6.mongodb.net/phonebook?retryWrites=true&w=majority";
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs);
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-    console.log(request.body)
-
-    blog.save().then(result => {
-        response.status(201).json(result);
-    });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
 });
